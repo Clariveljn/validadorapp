7b70e40c18bf8330a9ee072f10a79b0ca1534108
Clariveljn <cjeldres5@gmail.com>
2025-06-13 01:17:29 -0400

    first commit

---
+++ src/main/java/com/equipo/validador/App.java
@@ -0,0 +1,16 @@
+package com.equipo.validador;
+
+import java.util.logging.Logger;
+
+public class App {
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        String usuario = System.getenv("APP_USER");
+        if ("admin".equals(usuario)) {
+            logger.info("¡Bienvenido administrador!");
+        }
+    }
+}
+
+
